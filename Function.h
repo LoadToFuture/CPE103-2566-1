3435cf57fffb569f68acebfcf826a581cdc6669b
LoadToFuture <158584068+LoadToFuture@users.noreply.github.com>
2024-02-10 20:34:49 +0700

    Delete Function.h

--- Function.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FUNCTION_H_
-#define _FUNCTION_H_
-
-int summation(int m, int n);
-int Product(int m, int n);
-float Poly2(float a2, float a1, float a0, float x);
-float maxFloat(float data[], int n);
-
-#endif
